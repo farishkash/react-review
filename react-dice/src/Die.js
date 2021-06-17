67ca05a6bcb1cc471dc24e2948316e21d9b29fe0
Farish Kashefinejad <farishkash@hotmail.com>
2021-06-16 18:11:56 -0700

    fixes and review solution

--- react-dice/src/Die.js
+++ react-dice/src/Die.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./Die.css";
 
 function Die(props) {
-  return <i className={`Die fas fa-dice-`}></i>;
+  return (
+    <i
+      className={`Die fas fa-dice-${props.face} ${
+        props.rolling ? "shaking" : ""
+      }`}
+    ></i>
+  );
 }
 
 export default Die;
--- react-dice/src/RollDice.js
+++ react-dice/src/RollDice.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Die from "./Die";
 import "./RollDice.css";
 
 // setup the sides for six sided dice
 const sides = ["one", "two", "three", "four", "five", "six"];
 function RollDice() {
+  const [diceState, setDiceState] = useState({
+    die1: "one",
+    die2: "one",
+    rolling: false,
+  });
+
+  const handleRoll = () => {
+    const newDie1 = sides[Math.floor(Math.random() * sides.length)];
+    const newDie2 = sides[Math.floor(Math.random() * sides.length)];
+    const newDiceState = { die1: newDie1, die2: newDie2, rolling: false };
+    setDiceState({ ...diceState, rolling: true });
+    setTimeout(() => {
+      setDiceState(newDiceState);
+    }, 1000);
+  };
+
   return (
     <div className="RollDice">
-      <div className="RollDiceContainer"></div>
+      <div className="RollDiceContainer">
+        <Die face={diceState.die1} rolling={diceState.rolling} />
+        <Die face={diceState.die2} rolling={diceState.rolling} />
+      </div>
+      <button onClick={handleRoll} disabled={diceState.rolling}>
+        {diceState.rolling ? "Rolling" : "Roll Dice!"}
+      </button>
     </div>
   );
 }
